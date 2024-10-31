9a5b6c59d12b7da82ef6ce130b4fb1f0e302e0b7
SergDzhus <SDzhoos@gmail.com>
2024-10-31 11:56:59 +0300

    Заготовка для тестирования смс.

---
+++ main.py
@@ -0,0 +1,17 @@
+import  requests
+
+user='SDzhus'
+password='12Policiy'
+sender='SDzhus'
+receiver='79165237058'
+text='Hello, World!'
+
+url=f'https://my3.webcom.mobi/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={receiver}&text={text}'
+print(url)
+response = request.get(url)
+print(response)
+
+if response.status_code == 200:
+    print('Сообщение успешно отправлено!')
+else:
+    print(f'Ошибка при отправке сообщения {response.status_code}')
