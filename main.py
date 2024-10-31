bd2c641c7e9afebbf9a9de87f83ee827ed05f654
SergDzhus <SDzhoos@gmail.com>
2024-10-31 20:34:11 +0300

    Добавлен интерфей.

--- main.py
+++ main.py
@@ -1,54 +1,74 @@
 import re
 import requests
 import json
+from tkinter import *
+from tkinter import messagebox as mb
+
 
 def check_balance(login, password):
     url = 'https://my3.webcom.mobi/json/balance.php'
     headers = {'Content-type': 'text/json; charset=utf-8'}
 
-    data = {'login': login, 'password': password}
+    data = {'login': login,
+            'password': password}
 
     try:
         response = requests.post(url, data=json.dumps(data), headers=headers)
-
         if response.status_code == 200:
             response_data = response.json()
-            print(response_data)
-            print(f'Баланс: {response_data['money']} руб.')
+            return response_data['money']
         else:
-            print(f'Произошла ошибка {response.status_code}')
+            mb.showerror('Ошибка',f'Произошла ошибка проверки баланса {response.status_code}')
+            return None
     except Exception as e:
-        print(f'Произошла ошибка {e}')
+        mb.showerror('Ошибка',f'Произошла ошибка при проверке баланса {e}')
 
 
 def validate_phone_number(phone_number):
     pattern = r'^79\d{9}$'
     return bool(re.match(pattern, phone_number))
 
-user='SDzhus'
-password='12Policiy'
-sender='SDzhus'
-receiver='79165237058'
-text='Hello, World!'
 
-balance = check_balance(user, password)
-if balance:
-    if balance > 10:
-        if not validate_phone_number(receiver):
-            print('Ошибка! Некорректный номер телефона!')
+def send_sms():
+    user='SDzhus'
+    password='12Policiy'
+    sender='SDzhus'
+    receiver = receiver_entry.get()
+    text = text_entry.get()
+
+    balance = check_balance(user, password)
+    if balance:
+        if float(balance) > 10:
+            if not validate_phone_number(receiver):
+                mb.showinfo('Ошибка!', 'Некорректный номер телефона!')
+            else:
+                url = f'https://my3.webcom.mobi/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={receiver}&text={text}'
+                try:
+                    response = requests.get(url)
+                    if response.status_code == 200:
+                        mb.showinfo('Все хорошо', 'Сообщение успешно отправлено!')
+                    else:
+                        mb.showerror('Ошибка', f'Ошибка при отправке сообщения {response.status_code}')
+                except Exception as e:
+                    mb.showerror('Ошибка',f'Непредвиденная ошибка с кодом {e}')
         else:
-            url = f'https://my3.webcom.mob/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={receiver}&text={text}'
-            print(url)
-            try:
-                response = requests.get(url)
-                print(response)
-                if response.status_code == 200:
-                    print('Сообщение успешно отправлено!')
-                else:
-                    print(f'Ошибка при отправке сообщения {response.status_code}')
-            except Exception as e:
-                print(f'Непредвиденная ошибка с кодом {e}')
+            mb.showerror('Ошибка','Недостаточно средств!')
     else:
-        print('Недостаточно средств!')
-else:
-    print('Не удалось получить информацию о балансе!')
+        mb.showerror('Ошибка','Не удалось получить информацию о балансе!')
+
+window = Tk()
+window.title('Отпрака sms')
+window.geometry('250x110')
+
+Label(text='Номер получателя:').pack()
+receiver_entry = Entry()
+receiver_entry.pack()
+
+Label(text='Ввидите текст SMS:').pack()
+text_entry = Entry()
+text_entry.pack()
+
+send_button = Button(text='Отправить SMS', command=send_sms)
+send_button.pack()
+
+window.mainloop()
