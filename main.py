3c980d52b7826d24ee174587c8930a9f84c15a90
SergDzhus <SDzhoos@gmail.com>
2024-10-31 12:46:01 +0300

    Добавлена проверка корректности номера телефона с помощью регулярного выражения..

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from traceback import print_tb
-
 import  requests
+import re
+
+
+def validate_phone_number(phone_number):
+    pattern = r'^\d{11}$'
+    return bool(re.match(pattern, phone_number))
 
 user='SDzhus'
 password='12Policiy'
@@ -8,9 +12,12 @@ sender='SDzhus'
 receiver='79165237058'
 text='Hello, World!'
 
-url=f'https://my3.webcom.mobi/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={receiver}&text={text}'
-print(url)
-try:
+if not validate_phone_number(receiver):
+    print('Ошибка! Некорректный номер телефона!')
+else:
+    url = f'https://my3.webcom.mobi/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={receiver}&text={text}'
+    print(url)
+    try:
         response = requests.get(url)
         print(response)
 
@@ -18,5 +25,5 @@ try:
             print('Сообщение успешно отправлено!')
         else:
             print(f'Ошибка при отправке сообщения {response.status_code}')
-except Exception as e:
-    print(f'Непредвиденная ошибка с кодом {e}')
+    except Exception as e:
+        print(f'Непредвиденная ошибка с кодом {e}')
